2f0e461990143b0d18b46b5afa5547c8f9b013f9
Cynthia Pan <cynthiapan11@gmail.com>
2021-03-13 15:08:14 -0800

    worked on create event flow

---
+++ app/src/main/java/com/egghead/events/CreateEventFragment.kt
@@ -0,0 +1,30 @@
+package com.egghead.events
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class CreateEventFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_create_event, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<Button>(R.id.submit_button).setOnClickListener {
+            val action = R.id.action_createEventFragment_to_loggedInEventFeedFragment
+            findNavController().navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/egghead/events/LoggedInEventFeedFragment.kt
@@ -0,0 +1,35 @@
+package com.egghead.events
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_logged_in_event_feed.*
+
+class LoggedInEventFeedFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_logged_in_event_feed, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        create_event_button.setOnClickListener {
+            val action = R.id.action_loggedInEventFeedFragment_to_createEventFragment
+            findNavController().navigate(action)
+        }
+    }
+}

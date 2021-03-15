f186eea10af75ce00614c43b714b906d62cd8190
Cynthia Pan <cynthiapan11@gmail.com>
2021-03-15 13:24:16 -0700

    added create + update post flows

--- app/src/main/java/com/egghead/events/EventFeedFragment.kt
+++ app/src/main/java/com/egghead/events/EventFeedFragment.kt
@@ -8,13 +8,23 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_event_feed.*
+import java.util.*
 
 class EventFeedFragment : Fragment() {
 
-    private lateinit var firebase : FirebaseAuth
+    private lateinit var auth : FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        auth = FirebaseAuth.getInstance()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +32,6 @@ class EventFeedFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_event_feed, container, false)
-        firebase = FirebaseAuth.getInstance()
         val user = FirebaseAuth.getInstance().currentUser.isAnonymous
         if (user) {
             view.findViewById<FloatingActionButton>(R.id.favorite_button).hide()
@@ -34,12 +43,26 @@ class EventFeedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
+        val signoutButton = view.findViewById<Button>(R.id.signout_button)
+        signoutButton.setOnClickListener {
+            auth.signOut()
 
-       create_event_button.setOnClickListener {
+            val action = R.id.action_eventFeedFragment_to_signinFragment
+            findNavController().navigate(action)
+        }
+
+        val eventsRecyclerView: RecyclerView = view.findViewById(R.id.events_recycler_view)
+        eventsRecyclerView.layoutManager = LinearLayoutManager(this.context)
+
+        val eventListAdapter = EventListAdapter(arrayListOf(), findNavController())
+        eventsRecyclerView.adapter = eventListAdapter
+
+        EventFirestore.getAllEvents {
+            eventListAdapter.setData(it)
+        }
+
+        create_event_button.setOnClickListener {
             val action = R.id.action_eventFeedFragment_to_createEventFragment
             findNavController().navigate(action)
         }

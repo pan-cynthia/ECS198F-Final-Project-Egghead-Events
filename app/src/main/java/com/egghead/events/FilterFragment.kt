6b0697aa4444ad8a1b903db83533f33f721da006
Tina <twyoung@ucdavis.edu>
2021-03-16 16:52:51 -0700

    pop up? filter_pop_up.xml added

---
+++ app/src/main/java/com/egghead/events/FilterFragment.kt
@@ -0,0 +1,25 @@
+package com.egghead.events
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.auth.FirebaseAuth
+
+class FilterFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.filter_pop_up, container, false)
+        return view
+    }
+}

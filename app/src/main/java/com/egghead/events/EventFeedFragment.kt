85ca33779f57d2f9bedad7d486bf8e4e9faed933
Tina <twyoung@ucdavis.edu>
2021-03-16 18:16:49 -0700

    PopUp for filter added

--- app/src/main/java/com/egghead/events/EventFeedFragment.kt
+++ app/src/main/java/com/egghead/events/EventFeedFragment.kt
@@ -1,10 +1,15 @@
 package com.egghead.events
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.ui.window.isPopupLayout
 import androidx.navigation.fragment.findNavController
@@ -16,12 +21,17 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_event_feed.*
+import java.text.SimpleDateFormat
 import java.util.*
 
 class EventFeedFragment : Fragment() {
 
     private lateinit var auth : FirebaseAuth
     private var favoriteFilter: Boolean = false
+
+    private lateinit var alertDialog: AlertDialog
+    var startTimeInMilliseconds : Long = 0
+    var endTimeInMilliseconds : Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -116,7 +126,77 @@ class EventFeedFragment : Fragment() {
 
         val filterButton = view.findViewById<Button>(R.id.filterfilterbutton)
         filterButton.setOnClickListener {
+            val inflater: LayoutInflater = this.getLayoutInflater()
+            val dialogView: View = inflater.inflate(R.layout.filter_pop_up, null)
+
+            val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(this.requireContext())
+            dialogBuilder.setOnDismissListener(object : DialogInterface.OnDismissListener {
+                override fun onDismiss(arg0: DialogInterface) {
+                }
+            })
+            dialogBuilder.setView(dialogView)
+            alertDialog = dialogBuilder.create();
+            //alertDialog.window!!.getAttributes().windowAnimations =
+            alertDialog.show()
+
+            val start = alertDialog.findViewById<TextView>(R.id.filter_time_start)
+            start?.setOnClickListener {
+                pickDateTime(start, alertDialog)
+            }
+
+            val end = alertDialog.findViewById<TextView>(R.id.filter_time_end)
+            end?.setOnClickListener {
+                pickDateTime(end, alertDialog)
+            }
+
+
+            val filtersearch = alertDialog.findViewById<Button>(R.id.filter_search_button)
+            filtersearch?.setOnClickListener {
+                val keywords = alertDialog.findViewById<TextInputEditText>(R.id.key_words)?.text.toString()
+                val location = alertDialog.findViewById<TextInputEditText>(R.id.filter_location)?.text.toString()
+                eventListAdapter.setDataWithFilter(EventsSingleton.events, keywords, location, startTimeInMilliseconds, endTimeInMilliseconds)
+                alertDialog.hide()
+            }
 
         }
+    }
+    private fun pickDateTime(textView: TextView, alertDialog: AlertDialog) {
+        val textView = textView
+        val currentDateTime = Calendar.getInstance()
+        val startYear = currentDateTime.get(Calendar.YEAR)
+        val startMonth = currentDateTime.get(Calendar.MONTH)
+        val startDay = currentDateTime.get(Calendar.DAY_OF_MONTH)
+        val startHour = currentDateTime.get(Calendar.HOUR_OF_DAY)
+        val startMinute = currentDateTime.get(Calendar.MINUTE)
+
+        DatePickerDialog(
+            requireContext(),
+            DatePickerDialog.OnDateSetListener { _, year, month, day ->
+                TimePickerDialog(
+                    requireContext(),
+                    TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                        val pickedDateTime = Calendar.getInstance()
+                        pickedDateTime.set(year, month, day, hour, minute)
+                        if (textView == alertDialog?.findViewById<TextView>(R.id.filter_time_start)) {
+                            textView.text = SimpleDateFormat("MMM dd yyyy HH:mm", Locale.US).format(
+                                pickedDateTime.time
+                            )
+                            startTimeInMilliseconds = pickedDateTime.timeInMillis
+                        } else {
+                            textView.text = SimpleDateFormat("MMM dd yyyy HH:mm", Locale.US).format(
+                                pickedDateTime.time
+                            )
+                            endTimeInMilliseconds = pickedDateTime.timeInMillis
+                        }
+                    },
+                    startHour,
+                    startMinute,
+                    true
+                ).show()
+            },
+            startYear,
+            startMonth,
+            startDay
+        ).show()
     }
 }
